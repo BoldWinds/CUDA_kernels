037270c74bccbb6cb3ba45da562e49f08f078898
Bowen Liu <88589340+BoldWinds@users.noreply.github.com>
2025-09-07 23:49:30 +0800

    update data generate and timer

---
+++ include/cuda_utils.cuh
@@ -0,0 +1,70 @@
+#pragma once
+#include <iostream>
+#include <cuda_runtime.h>
+#include <curand.h>
+#include <curand_kernel.h>
+#include <cuda_fp16.h>
+
+inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort = true) {
+    if (code != cudaSuccess) {
+        fprintf(stderr, "GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
+        if (abort) exit(code);
+    }
+}
+#define CUDA_CHECK(ans) { gpuAssert((ans), __FILE__, __LINE__); }
+
+template<typename T>
+void generateRandomData(T* d_data, size_t n, unsigned long long seed = 0);
+
+template<>
+void generateRandomData<float>(float* d_data, size_t n, unsigned long long seed) {
+    curandGenerator_t gen;
+    curandCreateGenerator(&gen, CURAND_RNG_PSEUDO_DEFAULT);
+    curandSetPseudoRandomGeneratorSeed(gen, seed);
+    curandGenerateUniform(gen, d_data, n);
+    curandDestroyGenerator(gen);
+}
+
+template<>
+void generateRandomData<double>(double* d_data, size_t n, unsigned long long seed) {
+    curandGenerator_t gen;
+    curandCreateGenerator(&gen, CURAND_RNG_PSEUDO_DEFAULT);
+    curandSetPseudoRandomGeneratorSeed(gen, seed);
+    curandGenerateUniformDouble(gen, d_data, n);
+    curandDestroyGenerator(gen);
+}
+
+template<>
+void generateRandomData<int>(int* d_data, size_t n, unsigned long long seed) {
+    curandGenerator_t gen;
+    curandCreateGenerator(&gen, CURAND_RNG_PSEUDO_DEFAULT);
+    curandSetPseudoRandomGeneratorSeed(gen, seed);
+    curandGenerate(gen, reinterpret_cast<unsigned int*>(d_data), n);
+    curandDestroyGenerator(gen);
+}
+
+__global__ void floatToHalfKernel(__half *out, const float *in, size_t n) {
+    size_t idx = blockIdx.x * blockDim.x + threadIdx.x;
+    if (idx < n) {
+        out[idx] = __float2half(in[idx]);
+    }
+}
+
+template<>
+void generateRandomData<__half>(__half* d_data, size_t n, unsigned long long seed) {
+    float* d_temp_floats;
+    CUDA_CHECK(cudaMalloc(&d_temp_floats, n * sizeof(float)));
+
+    curandGenerator_t gen;
+    curandCreateGenerator(&gen, CURAND_RNG_PSEUDO_DEFAULT);
+    curandSetPseudoRandomGeneratorSeed(gen, seed);
+    generateRandomData<float>(d_temp_floats, n, seed);
+    curandDestroyGenerator(gen);
+
+    int threadsPerBlock = 256;
+    int blocksPerGrid = (n + threadsPerBlock - 1) / threadsPerBlock;
+    floatToHalfKernel<<<blocksPerGrid, threadsPerBlock>>>(d_data, d_temp_floats, n);
+    CUDA_CHECK(cudaGetLastError());
+
+    CUDA_CHECK(cudaFree(d_temp_floats));
+}
--- include/timer.h
+++ include/timer.h
@@ -1,25 +1,50 @@
 #pragma once
 
-#include "cuda_utils.h"
+#include "cuda_utils.cuh"
+#include <chrono>
+
+class Timer {
+public:
+    void start() {
+        start_ = std::chrono::high_resolution_clock::now();
+    }
+
+    void stop() {
+        end_ = std::chrono::high_resolution_clock::now();
+    }
+
+    double elapsed() const {
+        return std::chrono::duration<double, std::milli>(end_ - start_).count();
+    }
+
+    double clear() {
+        auto duration = elapsed();
+        start_ = end_;
+        return duration;
+    }
+
+private:
+    std::chrono::high_resolution_clock::time_point start_, end_;
+};
 
 class CudaTimer {
 public:
     CudaTimer() {
-        CHECK_CUDA_ERROR(cudaEventCreate(&start_));
-        CHECK_CUDA_ERROR(cudaEventCreate(&stop_));
+        CUDA_CHECK(cudaEventCreate(&start_));
+        CUDA_CHECK(cudaEventCreate(&stop_));
     }
     ~CudaTimer() {
-        CHECK_CUDA_ERROR(cudaEventDestroy(start_));
-        CHECK_CUDA_ERROR(cudaEventDestroy(stop_));
+        CUDA_CHECK(cudaEventDestroy(start_));
+        CUDA_CHECK(cudaEventDestroy(stop_));
     }
     void start() {
-        CHECK_CUDA_ERROR(cudaEventRecord(start_, 0));
+        CUDA_CHECK(cudaEventRecord(start_, 0));
     }
     float stop() {
         float elapsed_ms;
-        CHECK_CUDA_ERROR(cudaEventRecord(stop_, 0));
-        CHECK_CUDA_ERROR(cudaEventSynchronize(stop_));
-        CHECK_CUDA_ERROR(cudaEventElapsedTime(&elapsed_ms, start_, stop_));
+        CUDA_CHECK(cudaEventRecord(stop_, 0));
+        CUDA_CHECK(cudaEventSynchronize(stop_));
+        CUDA_CHECK(cudaEventElapsedTime(&elapsed_ms, start_, stop_));
         return elapsed_ms;
     }
 private:
