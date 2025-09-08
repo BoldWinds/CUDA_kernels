9edc5b19fc41a0b95addb21a45b050b894d3f69c
Bowen Liu <88589340+BoldWinds@users.noreply.github.com>
2025-09-08 14:13:52 +0800

    add elementwise

--- src/elementwise/elementwise.cu
+++ src/elementwise/elementwise.cu
@@ -1,5 +1,90 @@
 #include "timer.h"
 
+// one thread one element
+__global__ void elementwise_add_naive(const float* a, const float* b, float* c, unsigned n) {
+    unsigned idx = blockIdx.x * blockDim.x + threadIdx.x;
+    if (idx < n) {
+        c[idx] = a[idx] + b[idx];
+    }
+}
+
+// one thread four elements
+__global__ void elementwise_add_four(const float* a, const float* b, float* c, unsigned n) {
+    unsigned idx = blockIdx.x * blockDim.x + threadIdx.x;
+    unsigned total_threads = blockDim.x * gridDim.x;
+    if (idx < n) {
+        c[idx] = a[idx] + b[idx];
+        c[idx + total_threads] = a[idx + total_threads] + b[idx + total_threads];
+        c[idx + 2 * total_threads] = a[idx + 2 * total_threads] + b[idx + 2 * total_threads];
+        c[idx + 3 * total_threads] = a[idx + 3 * total_threads] + b[idx + 3 * total_threads];
+    }
+}
+
+// one thread four elements but vectorize
+__global__ void elementwise_add_vectorize(float* a, float* b, float* c, unsigned n) {
+    unsigned idx = 4 * (blockIdx.x * blockDim.x + threadIdx.x);
+    if (idx < n) {
+        float4 reg_a = (reinterpret_cast<float4 *>(a + idx))[0];
+        float4 reg_b = (reinterpret_cast<float4 *>(b + idx))[0];
+        float4 reg_c;
+        reg_c.x = reg_a.x + reg_b.x;
+        reg_c.y = reg_a.y + reg_b.y;
+        reg_c.z = reg_a.z + reg_b.z;
+        reg_c.w = reg_a.w + reg_b.w;
+        (reinterpret_cast<float4 *>(c + idx))[0] = reg_c;
+    }
+}
+
 int main(){
+    float *a, *b, *c;
+    const unsigned n = 1<<30;
+    CUDA_CHECK(cudaMalloc(&a, n * sizeof(float)));
+    CUDA_CHECK(cudaMalloc(&b, n * sizeof(float)));
+    CUDA_CHECK(cudaMalloc(&c, n * sizeof(float)));
+    Timer* timer = new Timer();
+    
+    // ---------------------------------
+    // naive
+    // ---------------------------------
+    generateRandomData(a, n);
+    generateRandomData(b, n);
+    int threadsPerBlock = 256;
+    int blocksPerGrid = (n + threadsPerBlock - 1) / threadsPerBlock;
+    timer->start();
+    elementwise_add_naive<<<blocksPerGrid, threadsPerBlock>>>(a, b, c, n);
+    CUDA_CHECK(cudaDeviceSynchronize());
+    timer->stop();
+    std::cout << "naive: " << timer->elapsed() << std::endl;
+
+    // ---------------------------------
+    // 一个线程处理多个数据
+    // ---------------------------------
+    generateRandomData(a, n);
+    generateRandomData(b, n);
+    threadsPerBlock = 256;
+    blocksPerGrid = (n + threadsPerBlock * 4 - 1) / (threadsPerBlock * 4);
+    timer->start();
+    elementwise_add_four<<<blocksPerGrid, threadsPerBlock>>>(a, b, c, n);
+    CUDA_CHECK(cudaDeviceSynchronize());
+    timer->stop();
+    std::cout << "four: " << timer->elapsed() << std::endl;
+
+    // ---------------------------------
+    // 一个线程处理多个数据(向量化)
+    // ---------------------------------
+    generateRandomData(a, n);
+    generateRandomData(b, n);
+    threadsPerBlock = 256;
+    blocksPerGrid = (n + threadsPerBlock * 4 - 1) / (threadsPerBlock * 4);
+    timer->start();
+    elementwise_add_vectorize<<<blocksPerGrid, threadsPerBlock>>>(a, b, c, n);
+    CUDA_CHECK(cudaDeviceSynchronize());
+    timer->stop();
+    std::cout << "vectorize: " << timer->elapsed() << std::endl;
+
+    CUDA_CHECK(cudaFree(a));
+    CUDA_CHECK(cudaFree(b));
+    CUDA_CHECK(cudaFree(c));
+
     return 0;
 }
---
+++ src/elementwise/elementwise.md
@@ -0,0 +1,61 @@
+# elementwise
+
+## elementwise_naive
+
+```cuda
+__global__ void elementwise_add_naive(const float* a, const float* b, float* c, unsigned n) {
+    unsigned idx = blockIdx.x * blockDim.x + threadIdx.x;
+    if (idx < n) {
+        c[idx] = a[idx] + b[idx];
+    }
+}
+```
+
+最简单的按元素加法kernel，对于$2^{30}$的单精度数据，15ms计算完毕。计算吞吐率15%，内存吞吐率94%，每个thread使用16个寄存器，在理论100%的利用率下达成了78%的利用率。
+怎么优化？
+1. 这个kernel是内存受限的
+2. 对于3090，一个thread使用40个以下的寄存器是可以打到理论100的占用率的，所以要一个thread计算更多的数据
+
+## elementwise_four
+
+```cuda
+__global__ void elementwise_add_four(const float* a, const float* b, float* c, unsigned n) {
+    unsigned idx = blockIdx.x * blockDim.x + threadIdx.x;
+    unsigned total_threads = blockDim.x * gridDim.x;
+    if (idx < n) {
+        c[idx] = a[idx] + b[idx];
+        c[idx + total_threads] = a[idx + total_threads] + b[idx + total_threads];
+        c[idx + 2 * total_threads] = a[idx + 2 * total_threads] + b[idx + 2 * total_threads];
+        c[idx + 3 * total_threads] = a[idx + 3 * total_threads] + b[idx + 3 * total_threads];
+    }
+}
+```
+
+一个thread处理四个元素。这样做提升了10%左右的计算利用率，但是性能几乎没有任何提升，为什么？
+因为这个kernel的瓶颈在于把数据从全局内存中读取然后再写入全局内存的过程，而并不是计算的过程。继续优化就应当注重别的方面：这个按元素加法怎么配合别的kernel？能不能使用半精度？因为半精度相当于读写同样大小的全局内存但是处理了两倍的数据。
+如果实在想对这个kernel进行提升，还可以向量化
+
+## elementwise_vectorize
+
+```cuda
+__global__ void elementwise_add_vectorize(float* a, float* b, float* c, unsigned n) {
+    unsigned idx = 4 * (blockIdx.x * blockDim.x + threadIdx.x);
+    if (idx < n) {
+        float4 reg_a = (reinterpret_cast<float4 *>(a + idx))[0];
+        float4 reg_b = (reinterpret_cast<float4 *>(b + idx))[0];
+        float4 reg_c;
+        reg_c.x = reg_a.x + reg_b.x;
+        reg_c.y = reg_a.y + reg_b.y;
+        reg_c.z = reg_a.z + reg_b.z;
+        reg_c.w = reg_a.w + reg_b.w;
+        (reinterpret_cast<float4 *>(c + idx))[0] = reg_c;
+    }
+}
+```
+
+然而是从结果是，没有实质的证据能证明向量化能带来什么性能提升
+
+
+## 总结
+
+以上的几个写法，在最终性能上几乎没有差别，对于这种简单的计算，就不要费心思优化kernel本身了
